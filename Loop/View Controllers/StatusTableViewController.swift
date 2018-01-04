0d8f275de150690c462a238c79a69267493817c0
Ken Stack <kenstack@yahoo.com>
2018-01-04 16:45:40 -0500

    fix loop data manager back to original in master

--- Loop/View Controllers/StatusTableViewController.swift
+++ Loop/View Controllers/StatusTableViewController.swift
@@ -594,7 +594,20 @@ final class StatusTableViewController: ChartsTableViewController {
             }
         }
     }
-
+    
+    private var remoteTempTargetMode: Bool? = nil {
+        didSet {
+            guard oldValue != remoteTempTargetMode else {
+                return
+            }
+            
+//            if let workoutMode = workoutMode {
+//                toolbarItems![6] = createWorkoutButtonItem(selected: workoutMode)
+//            } else {
+//                toolbarItems![6].isEnabled = false
+//            }
+        }
+    }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
